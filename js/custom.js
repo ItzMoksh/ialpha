b97c316ac0fd1b8c454e5d4d18481c6c81cd67c5
Sheikrashool <143116105+Sheikrashool@users.noreply.github.com>
2023-08-28 14:18:13 +0400

    Update custom.js
    changed the google map

--- js/custom.js
+++ js/custom.js
@@ -11,7 +11,7 @@ getYear();
 
 function myMap() {
     var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
+        center: new google.maps.LatLng(8.167044, 77.439761),
         zoom: 18,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
